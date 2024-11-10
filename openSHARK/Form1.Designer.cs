5f599d749939786cd51b38d294c9d1ef9638b707
recepsyegin <recepsyegin@gmail.com>
2024-11-10 15:07:40 +0300

    Proje dosyası ekle.

---
+++ openSHARK/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace openSHARK
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.Smoothing = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.Smoothing.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(8, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Kamerayı aç";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(8, 70);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(540, 464);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(8, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 41);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Canny Edge Detection";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(8, 81);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(518, 444);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(8, 18);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 49);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Homogeneus Blur";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Smoothing
+            // 
+            this.Smoothing.Controls.Add(this.tabPage1);
+            this.Smoothing.Controls.Add(this.tabPage2);
+            this.Smoothing.Controls.Add(this.tabPage3);
+            this.Smoothing.Controls.Add(this.tabPage4);
+            this.Smoothing.Controls.Add(this.tabPage5);
+            this.Smoothing.Font = new System.Drawing.Font("Microsoft YaHei UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Smoothing.Location = new System.Drawing.Point(0, 1);
+            this.Smoothing.Name = "Smoothing";
+            this.Smoothing.SelectedIndex = 0;
+            this.Smoothing.Size = new System.Drawing.Size(1656, 648);
+            this.Smoothing.TabIndex = 7;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1197, 534);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.pictureBox6);
+            this.tabPage2.Controls.Add(this.pictureBox4);
+            this.tabPage2.Controls.Add(this.pictureBox3);
+            this.tabPage2.Controls.Add(this.pictureBox5);
+            this.tabPage2.Controls.Add(this.button6);
+            this.tabPage2.Controls.Add(this.button5);
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1648, 619);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "SMOOTHING";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.button2);
+            this.tabPage3.Controls.Add(this.pictureBox2);
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(1648, 619);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 25);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(1648, 619);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "tabPage4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Location = new System.Drawing.Point(4, 25);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage5.Size = new System.Drawing.Size(1197, 531);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "tabPage5";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(398, 21);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(84, 48);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Gaussian Blur";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(831, 23);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(84, 46);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Median Blur";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(1266, 23);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(88, 46);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Bilateral Blur";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(831, 110);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(344, 320);
+            this.pictureBox5.TabIndex = 12;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(8, 110);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(344, 320);
+            this.pictureBox3.TabIndex = 13;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click_1);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(398, 110);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(344, 320);
+            this.pictureBox4.TabIndex = 14;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(1266, 110);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(344, 320);
+            this.pictureBox6.TabIndex = 15;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1659, 787);
+            this.Controls.Add(this.Smoothing);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.Smoothing.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TabControl Smoothing;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
+
---
+++ openSHARK/Form1.cs
@@ -0,0 +1,282 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using OpenCvSharp.XImgProc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace openSHARK
+{
+    public partial class Form1 : Form
+    {
+
+        // KAMERA AÇMA İÇİN DEĞİŞKENLER
+        private VideoCapture capture;
+        private bool isCameraRunning = false;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (isCameraRunning)
+            {
+                // Kamera zaten çalışıyorsa durdur
+                isCameraRunning = false;
+                capture.Release(); // Kamerayı serbest bırak
+                button1.Text = "Kamerayı Aç";
+            }
+            else
+            {
+                // Kamera çalışmıyorsa başlat
+                capture = new VideoCapture(0); // 0, varsayılan kamera
+                isCameraRunning = true;
+                button1.Text = "Kamerayı Kapat";
+                
+
+                // Kameradan gelen görüntüyü sürekli güncellemek için bir döngü başlat
+                await Task.Run(() =>
+                {
+                    while (isCameraRunning && capture.IsOpened())
+                    {
+                        using (var frame = new Mat())
+                        {
+                            capture.Read(frame); // Kameradan bir kare al
+                            if (!frame.Empty())
+                            {
+                                // PictureBox'ta görüntülemek için formu güncelle
+                                pictureBox1.Image = BitmapConverter.ToBitmap(frame);
+                            }
+                        }
+                    }
+                });
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if(capture != null && capture.IsOpened())
+            {
+                // Kenar algılama için yeni bir döngü başlat
+                Task.Run(() =>
+                {
+                    while (isCameraRunning && capture.IsOpened())
+                    {
+                        using (var frame = new Mat())
+                        using (var edges = new Mat())
+                        {
+                            capture.Read(frame); // Kameradan bir kare al
+
+                            if(!frame.Empty())
+                            {
+
+                                // Kenar algılama işlemini uygula
+                                Cv2.Canny(frame, edges, 100, 200);
+
+                                // Orijinal Kareyi göster (pictureBox1)
+                                pictureBox1.Image = BitmapConverter.ToBitmap(frame);
+
+                                // Kenar algılanmış kareyi göster (pictureBox2)
+                                pictureBox2.Image = BitmapConverter.ToBitmap(edges);
+
+
+                                
+
+                            }
+                        }
+                    }
+                });
+            }
+        }
+        
+
+        // Homogeneus Blur
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            if (capture != null && capture.IsOpened())
+            {
+                isCameraRunning = true;
+
+                Task.Run(() =>
+                {
+                    while (isCameraRunning && capture.IsOpened())
+                    {
+                        using (var frame = new Mat())
+                        using (var smoothed = new Mat())
+                        {
+                            capture.Read(frame);
+
+                            if (!frame.Empty())
+                            {
+                                OpenCvSharp.Size size = new OpenCvSharp.Size(15, 15);
+                                Cv2.Blur(frame, smoothed, size);
+
+                                // Form ana iş parçacığına göndermek için Invoke kullanılır
+                                Invoke(new Action(() =>
+                                {
+                                    pictureBox3.Image?.Dispose();
+                                    pictureBox3.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(smoothed);
+                                }));
+                            }
+                        }
+                    }
+                });
+            }
+        }
+
+
+
+
+        // Canny Edge Detection Picture Box
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        // Smooth Images
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+       
+
+        // Gaussian Blur 
+        private void button4_Click(object sender, EventArgs e)
+        {
+           
+            
+            if(capture != null && capture.IsOpened())
+            {
+                isCameraRunning = true;
+                Task.Run(() =>
+                {
+                while (isCameraRunning && capture.IsOpened())
+                {
+                    using (var frame = new Mat())
+                    using (var gaussianBlurFrame = new Mat())
+                    {
+                        capture.Read(frame);
+                        if (!frame.Empty())
+                        {
+                            OpenCvSharp.Size kernelSize = new OpenCvSharp.Size(15, 15);
+                            Cv2.GaussianBlur(frame, gaussianBlurFrame, kernelSize, 0, 0);
+
+                            Invoke(new Action(() =>
+                                {
+                                pictureBox4.Image?.Dispose();
+                                pictureBox4.Image =
+                                OpenCvSharp.Extensions.BitmapConverter.ToBitmap(gaussianBlurFrame);
+                            }));
+                            
+                            }
+
+                        }
+                    }
+                });
+            }
+        }
+
+
+        // Median Blur Button
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if(capture!=null && capture.IsOpened())
+            {
+                //isCameraRunning = true;
+                Task.Run(() =>
+                {
+                    while(isCameraRunning && capture.IsOpened())
+                    {
+                        using (var frame = new Mat())
+                        using(var medianBlurFrame = new Mat())
+                        {
+                            capture.Read(frame);
+                            if(!frame.Empty())
+                            {
+                      
+                                Cv2.MedianBlur(frame, medianBlurFrame, 21); // must be odd size 1 3 5 7 ... 21 23 .. buraya trackbar ile deger degistirme eklenecek
+                                Invoke(new Action(() =>
+                                {
+                                    pictureBox5.Image?.Dispose();
+                                    pictureBox5.Image =
+                                    OpenCvSharp.Extensions.BitmapConverter.ToBitmap(medianBlurFrame);
+                                }));
+                            }
+                        }
+                    }
+                });
+            }
+        }
+
+
+        // Bilateral Blur Button
+        private void button6_Click(object sender, EventArgs e)
+        {
+            
+
+            if(capture != null && capture.IsOpened())
+            {
+                //isCameraRunning = true;
+                Task.Run(() =>
+                {
+                    while (isCameraRunning && capture.IsOpened())
+                    {
+                        using (var frame = new Mat())
+                        using (var bilateralBlurFrame = new Mat())
+                        {
+                            capture.Read(frame);
+                            if(!frame.Empty())
+                            {
+                                Cv2.BilateralFilter(frame, bilateralBlurFrame, 30, 30, 30);
+                                Invoke(new Action(() =>
+                                {
+                                    pictureBox6.Image?.Dispose();
+                                    pictureBox6.Image =
+                                    OpenCvSharp.Extensions.BitmapConverter.ToBitmap(bilateralBlurFrame);
+                                }));
+
+                            }
+                        }
+                    }
+                });
+            }
+        }
+
+        private void pictureBox3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+        // Gaussian Blur Picture Box
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+        // Median Blur Picture Box
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // Bilateral Blur Picture Box
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
